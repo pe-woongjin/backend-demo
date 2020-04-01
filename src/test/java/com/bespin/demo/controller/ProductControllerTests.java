dd0aa5cd8502fc22f89fa4f3e24ddc3db2e6e814
seonbo.shim <seonbo.shim@bespinglobal.com>
2020-04-01 19:38:52 +0900

    [CMSA-381] [웅진 Demo앱] Backend API 앱 프로젝트 구성 - spring-boot
    [Resolution]
    junit 5 테스트를 위한 maven dependency 추가.
    
    [DoD] JUnit Test 검증을 통해 통과됨을 확인
    
    [Team] Productivity Engineering
    [Company] BespinGlobal

---
+++ src/test/java/com/bespin/demo/controller/ProductControllerTests.java
@@ -0,0 +1,19 @@
+package com.bespin.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductControllerTests {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void list() {
+    }
+
+}
