363ed1908a67c355497a6e7c1ddeed4af2d0b81d
sanguk.kang <studymode83@gmail.com>
2020-04-06 15:30:10 +0900

    cors 에러 방지를 위한 webconfig 파일 셋팅

---
+++ src/main/java/com/bespin/demo/WebConfig.java
@@ -0,0 +1,20 @@
+package com.bespin.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOrigins("http://127.0.0.1:3000")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
+
